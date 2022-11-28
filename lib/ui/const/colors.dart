923f6e0641b5ff83ec6061d647b8b249c0602f42
myanes <myanes.dev@gmail.com>
2022-11-28 14:46:39 +0000

    improved light theme

---
+++ lib/ui/const/colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/painting.dart';
+
+class AppColors {
+  static const Color iosBgColor = Color(0xffF2F2F7);
+}
--- lib/ui/const/themes.dart
+++ lib/ui/const/themes.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_clean_architecture_2022/ui/const/colors.dart';
 
 class AppThemes {
-  static final ThemeData light = ThemeData.light();
+  static final ThemeData light = ThemeData.light().copyWith(
+    scaffoldBackgroundColor: AppColors.iosBgColor,
+    colorScheme: _lightColors,
+  );
   static final ThemeData dark = ThemeData.dark().copyWith(
     scaffoldBackgroundColor: Colors.black,
     colorScheme: _darkColors,
@@ -10,4 +14,9 @@ class AppThemes {
 
 final ColorScheme _darkColors = ThemeData.dark().colorScheme.copyWith(
       background: Colors.black,
+      onBackground: Colors.white,
+    );
+final ColorScheme _lightColors = ThemeData.light().colorScheme.copyWith(
+      background: AppColors.iosBgColor,
+      onBackground: Colors.black,
     );
